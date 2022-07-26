72e885ed9e6f7dda3db33f02296e75148b3d9d38
favour.adah <favour.adah@IC-CS233-9.uc.ucalgary.ca>
2022-07-26 11:13:12 -0600

    Added my branch label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Khondaker Samin Rashid's Label"));
+			root.getChildren().add(new Label("Favour's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Favour's project");
